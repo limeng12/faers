74e6e2a10a458677fd5648519ae4c6df9865b011
Meng Li <331060295@qq.com>
2015-06-01 17:32:03 -0400

    first commit

---
+++ R/connect.R
@@ -0,0 +1,18 @@
+connect<-function(){
+  #path="/home/limeng/workspace/DrugAnalysis/";
+  path=system.file(package = "faers");
+  init<-function(){
+    .jinit()
+    isInit<-TRUE;
+    .jaddClassPath(dir( paste(path,"/faers/",sep=""), full.names=TRUE ) );
+    .jaddClassPath(paste(path,"/faers.jar",sep="") );
+  }
+  
+  isInit<-FALSE
+  
+  if(!isInit)
+    init();
+  
+  J("main.ccbb.faers.core.DatabaseConnect")$setMysqlConnector( "rdc04.uits.iu.edu:3080","wxy","wxy","drug");
+  
+}
---
+++ R/getDrugIntersectIsr.R
@@ -0,0 +1,18 @@
+
+getDrugIntersectIsr<-function(inputDrugNames){
+  search<-J("main.ccbb.faers.core.Search")$getInstance();
+  cat( (search %instanceof% "main.ccbb.faers.core.Search") );
+  
+  drugNames<-.jnew("java.util.ArrayList");
+  #drugNames$add("LEPIRUDIN");  #drugNames$add("CETUXIMAB");
+  
+  for(i in 1:length(inputDrugNames) ) {
+    drugNames$add(inputDrugNames[i]);
+  }
+  
+  result<-search$intersectionSearchDrugsSIRUsingDrugBank(drugNames);
+  
+  isrs<-sapply( result, function(item) item$intValue() );
+  return(isrs);
+  
+}
