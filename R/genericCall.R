07fbb2b4042201abbcf801a2d9d8425ac461e9f5
Meng Li <331060295@qq.com>
2015-06-01 19:50:38 -0400

    generic

--- R/genericCall.R
+++ R/genericCall.R
@@ -23,15 +23,10 @@ genericCallSI<-function(inputName,functionName){
   search<-J("main.ccbb.faers.core.Search")$getInstance();
   cat( (search %instanceof% "main.ccbb.faers.core.Search") );
   
-  names<-.jnew("java.util.ArrayList");
-  #drugNames$add("LEPIRUDIN");  #drugNames$add("CETUXIMAB");
   
-  for(i in 1:length(inputNames) ) {
-    names$add(inputNames[i]);
-  }
   
   #result<-search$intersectionSearchDrugsSIRUsingDrugBank(names);
-  result<-eval( parse(text=paste("search$",functionName,"(names)",sep="") )  );
+  result<-eval( parse(text=paste("search$",functionName,"(inputName)",sep="") )  );
   
   isrs<-sapply( result, function(item) item$intValue() );
   
